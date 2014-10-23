9ad120308041b774a631b67477ba4e748599ace3
Baisheng Song <patricksong1993@gmail.com>
2014-10-23 20:44:22 +0100

    DT first push

---
+++ decision_tree/choose_best_decision_attribute.m
@@ -0,0 +1,5 @@
+function[best_attribute] = choose_best_decision_attribute(attributes, examples, binary_targets)
+
+%TODO
+
+end
---
+++ decision_tree/majority_value.m
@@ -0,0 +1,5 @@
+function[majority_value] = majority_value(binary_targets)
+
+majority_value = mode(binary_targets);
+
+end
