0878d786cca990a6c4418528c442154188c08de0
Baisheng Song <patricksong1993@gmail.com>
2014-10-28 00:59:14 +0000

    modified choose best attribute

--- decision_tree/decision_tree_learning.m
+++ decision_tree/decision_tree_learning.m
@@ -1,21 +1,27 @@
 function tree = decision_tree_learning(examples, attributes, binary_targets)
-    
-if(length(unique(binary_targets)) == 1)
+
+% All binary_targets are equal
+if (length(unique(binary_targets)) == 1)
     tree = make_node('',binary_targets(1));
-elseif (isempty(attributes))
+
+% No more attributes to expand
+elseif (sum(attributes) == 0)
     tree = make_node('',majority_value(binary_targets));
+
+% Normal case
 else
     best_attribute = choose_best_decision_attribute(examples, attributes, binary_targets);
     tree = make_node(best_attribute, '');
     for possible_value = 0:1
-        %row numbers with the best attribute equals to possible value.
+        %row numbers with the best attribute equals to possible value
         rows_on_best = find(examples(:,best_attribute) == possible_value);
         reduced_examples = examples(rows_on_best, :);
         if (isempty(reduced_examples))
             tree.kids{possible_value + 1} = make_node('', majority_value(binary_targets));
         else
             reduced_targets = binary_targets(rows_on_best);
-            tree.kids{possible_value + 1} = decision_tree_learning(reduced_examples, attributes(attributes ~= best_attribute), reduced_targets);
+            attributes(best_attribute) = 0;
+            tree.kids{possible_value + 1} = decision_tree_learning(reduced_examples, attributes, reduced_targets);
         end
     end
 end
@@ -27,3 +33,5 @@ function node = make_node(op, class)
     node.kids = {};
     node.class = class;
 end
+
+
