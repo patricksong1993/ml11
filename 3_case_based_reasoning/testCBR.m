8f03f495f4ac6ecc115e672cf2770667e160bc62
ybl11 <yan.b.liu11@imperial.ac.uk>
2014-11-26 02:35:23 +0000

    testCBR working, need to split datasets for nfoldvalidation in correct format

--- 3_case_based_reasoning/testCBR.m
+++ 3_case_based_reasoning/testCBR.m
@@ -1,4 +1,14 @@
 function predictions = testCBR(CBR, x2)
-
+    
+    no_x2 = size(x2,1);
+    
+    for i = 1:no_x2
+        [closestcase, distance, index] = retrieve(CBR, x2(i,:));
+        solvedcase = reuse(closestcase, x2(i,:));
+        %retain the solved case or not?
+        CBR = retain(CBR, solvedcase, distance, index);
+        %%%%%
+        predictions(i,1) = solvedcase.class;
+    end
 
 end
